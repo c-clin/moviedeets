46ee38845b690a210d960ede519cdac82128fc50
Catherine Lin <catherine.lin.93@gmail.com>
2018-06-08 14:52:41 -0700

    changed content for testing

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
