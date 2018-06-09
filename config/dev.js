96b4194183af2dacdd6b79bef70ba45fbf3de925
Catherine Lin <31521769+c-clin@users.noreply.github.com>
2018-06-08 17:59:13 -0700

    delete file

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-    '822356003327-um9sblnhbl37s2amdee3vk6vl9bnvo94.apps.googleusercontent.com',
-  googleClientSecret: 'wLN0zIaAC-ORdHgYZz4CzOdx',
-  mongoURI: 'mongodb://cat:catherine123@ds153700.mlab.com:53700/moviedeets',
-  cookieKey: 'lkjhgfdfghjkjhgfdfghjkiuytrfghytresdghfjnmkjhjkjh'
-};
